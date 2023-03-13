91998bc5848d2914b6e7470385e8bc3de2150e78
Nithin-Valiyaveedu <nithinvaliya@gmail.com>
2023-03-13 18:11:30 +0530

    added context api

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { createClient } from "@supabase/supabase-js";
-
+import UserContext from "../Context/User/user.context";
 const supabase = createClient(
   "https://dauawcdclgotidietdhl.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhdWF3Y2RjbGdvdGlkaWV0ZGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY3ODY0MDAsImV4cCI6MTk5MjM2MjQwMH0.P-7cX1eAP4H8UUAPpsQAAQMspiHBQJ6XipZsNsCI6EY"
 );
 
 const Landing = () => {
+  const user = useContext(UserContext);
+
   const loginWithDiscord = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "discord",
       options: {
-        redirectTo: "https://discord-web-app.onrender.com/success",
+        // redirectTo: "https://discord-web-app.onrender.com/success",
+        redirectTo: "http://localhost:3000/success",
       },
     });
   };
+  console.log(user);
 
   return (
     <div className="landingContainer">
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,25 +1,29 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+
+import UserContext from "./Context/User/user.context";
 import Landing from "./Components/Landing";
 import Success from "./Components/Success";
 
 const Layout = () => {
+  const currentUser = window.localStorage.getItem("userData");
+  const [user, setUser] = useState(JSON.parse(currentUser) || "");
   return (
     <>
-      <Routes>
-        {/* unprotected routes */}
-        <Route
-          path="/"
-          exact
-          element={<Landing />}
-        />
-        <Route
-          path="/success"
-          exact
-          element={<Success />}
-        />
-      </Routes>
+      <UserContext.Provider value={user}>
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={<Landing />}
+          />
+          <Route
+            path="/success"
+            exact
+            element={<Success setUser={setUser} />}
+          />
+        </Routes>
+      </UserContext.Provider>
     </>
   );
 };
