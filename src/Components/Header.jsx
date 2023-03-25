b6a213db4b943b120f5aef4d03c7a6e50d1f2294
Nithin-Valiyaveedu <nithinvaliya@gmail.com>
2023-03-25 12:44:05 +0530

    ui changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
       <div className="navbarContainer">
         <h1>Coencierge 2.0</h1>
         <div className="profileHead">
-          {/* <h1>N</h1> */}
-          <img
+          <h1>N</h1>
+          {/* <img
             src="https://images.unsplash.com/photo-1617854818583-09e7f077a156?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             className="profileHead"
             alt=""
-          ></img>
+          ></img> */}
         </div>
       </div>
     </div>
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -11,7 +11,7 @@ const Landing = () => {
     await supabase.auth.signInWithOAuth({
       provider: "discord",
       options: {
-        redirectTo: "https://discord-web-app.onrender.com/success",
+        redirectTo: "http://localhost:3000/success",
       },
     });
   };
